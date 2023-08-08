053229adfa5922f2799150aee82c89d51205271b
DanielKirshner <danielkirshner7878@gmail.com>
2023-08-08 22:03:34 +0200

    tested merger POC

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+from PyPDF2 import PdfMerger
+
+PDF_FILES: list[str] = ["tests\\first.pdf", "tests\\second.pdf", "tests\\third.pdf"]
+
+def logic():
+    merger = PdfMerger()
+    
+    for pdf in PDF_FILES:
+        merger.append(pdf)
+        
+    merger.write("tests\\merged.pdf")
+    merger.close()
 
 def main():
-    pass
+    logic()
 
 if __name__ == "__main__":
     main()
