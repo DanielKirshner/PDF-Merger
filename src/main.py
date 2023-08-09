5ec91380f68541e90a9eea59d036d4a697fe0954
DanielKirshner <danielkirshner7878@gmail.com>
2023-08-09 18:53:36 +0200

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-import merge.merge_logic as merge_logic
 from PyPDF2 import PdfMerger
+import merge_logic
 
 PDF_PATHS: list[str] = [".\\tests\\first.pdf", ".\\tests\\second.pdf", ".\\tests\\third.pdf"]
-OUTPUT_PDF_PATH = ".\\tests\\merged.pdf"
+OUTPUT_PDF_PATH: str = ".\\tests\\merged.pdf"
 
 def main():
     merge_logic.run_logic(PDF_PATHS, OUTPUT_PDF_PATH)
