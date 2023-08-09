982a90ce2fb1db5d4dba2dee26479e4aca39d558
DanielKirshner <danielkirshner7878@gmail.com>
2023-08-09 19:02:37 +0200

    Added file utils to check if a file is a pdf and exists + refactoring

--- src/main.py
+++ src/main.py
@@ -7,5 +7,6 @@ OUTPUT_PDF_PATH: str = ".\\tests\\merged.pdf"
 def main():
     merge_logic.run_logic(PDF_PATHS, OUTPUT_PDF_PATH)
 
+
 if __name__ == "__main__":
     main()
--- src/merge_logic.py
+++ src/merge_logic.py
@@ -2,13 +2,6 @@ from PyPDF2 import PdfMerger
 
 @staticmethod
 def run_logic(pdf_paths: list[str], output_merged_pdf_path: str) -> None:
-    """
-    Run merging logic
-
-    Args:
-        pdf_paths (list[str]): A list of all pdf paths to be merged
-        output_merged_pdf_path (str): Output pdf file path (will created)
-    """
     PDF_MERGER_THROW_EXCEPTIONS = False     # But it will log a warning message.
     merger = PdfMerger(strict=PDF_MERGER_THROW_EXCEPTIONS)
     
@@ -20,4 +13,3 @@ def run_logic(pdf_paths: list[str], output_merged_pdf_path: str) -> None:
     print(f"All files merged into {output_merged_pdf_path}")
     
     merger.close()
-    # print(f"File descriptors closed and memory usage cleaned up.")
