b15b74519407a318c1a4873dfb2c3a34661af53d
DanielKirshner <danielkirshner7878@gmail.com>
2023-08-12 18:06:53 +0300

    added exception handling

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from PyPDF2 import PdfMerger
 import merge_logic
+import traceback
 
-PDF_PATHS: list[str] = [".\\tests\\first.pdf", ".\\tests\\second.pdf", ".\\tests\\third.pdf"]
-OUTPUT_PDF_PATH: str = ".\\tests\\merged.pdf"
+PDF_PATHS: list[str] = [".//tests//first.pdf", ".//tests//second.pdf", ".//tests//third.pdf"]
+OUTPUT_PDF_PATH: str = ".//tests//merged.pdf"
 
 def main():
     merge_logic.run_logic(PDF_PATHS, OUTPUT_PDF_PATH)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as unhandled_exception:
+        print("Unhandled exception caught.")
