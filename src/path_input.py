9e33d28264d817421db377f252d64b38fc4edc37
DanielKirshner <danielkirshner7878@gmail.com>
2023-08-15 23:26:02 +0200

    added paths input

---
+++ src/path_input.py
@@ -0,0 +1,27 @@
+
+@staticmethod
+def get_path_from_user() -> str:
+    path = ''
+    path_valid = False
+    
+    while not path_valid:
+        print("Enter path -> ")
+        path = str(input())
+        
+        if not isinstance(path, str) or len(path) == 0:
+            print("Invalid path given.")
+        else:
+            path_valid = True
+    
+    return path
+
+@staticmethod
+def get_list_of_paths_from_user(num_of_paths: int) -> list[str]:
+    paths = []
+    for i in range(num_of_paths):
+        paths.append(get_path_from_user())
+    
+    return paths
+
+paths_list = get_list_of_paths_from_user(3)
+print(paths_list)
