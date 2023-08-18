e47c0bb1ce096d0a25a3d6ce55f144b0169a88fb
DanielKirshner <danielkirshner7878@gmail.com>
2023-08-18 15:01:26 +0200

    combined full output path

--- src/merge_logic.py
+++ src/merge_logic.py
@@ -1,22 +1,23 @@
 from PyPDF2 import PdfMerger
+
 from path_input import *
+from path_utils import combine_full_path
 
 
 @staticmethod
 def run_logic() -> None:
     pdf_paths = get_list_of_paths_from_user()
-    print("[cyan]Enter output path:")
+    print("[cyan]Enter output pdf file name:")
     OUTPUT_FILE_SHOULD_EXISTS = False
-    output_merged_pdf_path = get_path_from_user(OUTPUT_FILE_SHOULD_EXISTS)
-    
+    full_output_merged_pdf_path = combine_full_path(get_path_from_user(OUTPUT_FILE_SHOULD_EXISTS))
     PDF_MERGER_THROW_EXCEPTIONS = False
     merger = PdfMerger(strict=PDF_MERGER_THROW_EXCEPTIONS)
     
     for current_pdf in pdf_paths:
-        print(f"Appending {current_pdf} to {output_merged_pdf_path}")
+        print(f"Appending {current_pdf} to {full_output_merged_pdf_path}")
         merger.append(current_pdf)
         
-    merger.write(output_merged_pdf_path)
-    print(f"[green]All files merged into {output_merged_pdf_path}")
+    merger.write(full_output_merged_pdf_path)
+    print(f"[green]All files merged into {full_output_merged_pdf_path}")
     
     merger.close()
