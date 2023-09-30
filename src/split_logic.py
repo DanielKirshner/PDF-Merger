2609298518ac630a8c5647d3fe236654ddee6f9b
DanielKirshner <danielkirshner7878@gmail.com>
2023-09-30 19:27:20 +0300

    Testing split logic

--- src/split_logic.py
+++ src/split_logic.py
@@ -3,4 +3,24 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 @staticmethod
 def run_logic() -> None:
-    pass
+    page_indexes = [1, 2]
+    
+    with open("./out/merged.pdf", "rb") as f:
+        reader = PdfFileReader(f)
+        first_writer = PdfFileWriter()
+        second_writer = PdfFileWriter() # the rest pages
+        
+        for page_index in range(len(reader.pages)):
+            if page_index in page_indexes:
+                first_writer.addPage(reader.getPage(page_index))
+            else:
+                second_writer.addPage(reader.getPage(page_index))
+        
+        with open("./out/selected.pdf", "wb") as f2:
+            first_writer.write(f2)
+            
+        with open("./out/rest.pdf", "wb") as f2:
+            second_writer.write(f2)
+            
+run_logic()
+
